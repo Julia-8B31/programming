50b8f8c3cc05cf9087da8746562faa27e56ba89e
Yuliya A. Novikova <yan29@tpu.ru>
2024-02-10 14:14:46 +0700

    изменения1

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@
 
 
 int main(int, char**) {
-    int input_1, input_2 
-    std :: vector <int> vec 
-    std :: sin>> input_1
+    int input_1, input_2 ;
+    std :: vector <int> vec; 
+    std :: cout<<  "Введите число: " ;
+    std :: cin >> input_1;
     if (input_1 >100){
+        cout << input_1
 
     }
   
